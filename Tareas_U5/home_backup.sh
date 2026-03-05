ffe3c508a19653cc18201f1e8243b4abe8e35dcc
P-arsimony <trooper0829@gmail.com>
2026-03-04 21:48:48 -0600

    Script para respaldo de C:\Users\troop para la tarea de U5

--- Tareas_U5/home_backup.sh
+++ Tareas_U5/home_backup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Creamos un directorio dentro de $HOME llamado backups
+backups="$HOME/backups" 
+mkdir -p "$backups" # Directorio general de respaldos
+
+backup_dir="$backups/backup_$(date +%Y%m%d)"
+mkdir -p "$backup_dir" # Directorios por fecha del día en que se realizó el respaldo
+
 # Copiar archivos y directorios de la variable $HOME
-backup_dir="$HOME/backup_$(date +%Y%m%d)"
-mkdir -p "$HOME/backup"
+cp -a "$HOME/." "$backup_dir/"
 
-for homedir in "$HOME"/*; 
-do 
-    cp -a --backup $HOME "$backup_dir";
-done
+7z a "$backups/backup_$(date +%Y%m%d).7z" "$backup_dir"
 
-7z a "backup_$(date +%Y%m%d).7z" "$backup_dir"
-
+rm -rf "$backup_dir"
 
